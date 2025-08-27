df8dec326e0304b076c01caa269353faab6ccf65
sabriemin <sabri.emin97@gmail.com>
2025-08-27 13:34:48 +0300

    Delete assets/js directory

--- assets/js/checklist-react.compiled.js
+++
@@ -1,4 +0,0 @@
-// Checklist React App (compiled placeholder)
-// Normalde burada Babel derlenmiş kod olacak.
-// Geliştirme ortamında `npx babel checklist-react.js --out-file checklist-react.compiled.js --presets=@babel/preset-react` ile derleyebilirsiniz.
-console.log("Checklist React compiled JS yükleniyor...");
